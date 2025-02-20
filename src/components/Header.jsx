e9b7c56f99b568c4d515ca8d35b70b8d0f9e7830
noamguterman <noamguterman@gmail.com>
2025-02-20 10:49:35 +0200

    Add meta description to index.html and improve alt text for logo in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import chefClaudeLogo from "../images/chef-claude-icon.png"
 export default function Header() {
     return (
         <header>
-            <img src={chefClaudeLogo}/>
+            <img src={chefClaudeLogo} alt="Chef Claude logo"/>
             <h1>Chef Claude</h1>
         </header>
     )
